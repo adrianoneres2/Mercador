de56c96a7a1fd0d47ed214acb9eb3c42cf89f467
adrianoneres2 <adrianoneres2@gmail.com>
2017-10-11 00:13:51 -0300

    remove tudo

--- src/main/java/br/com/ans/dao/AutenticadorDao.java
+++
@@ -1,8 +0,0 @@
-package br.com.ans.dao;
-
-import br.com.ans.model.Usuario;
-
-public interface AutenticadorDao {
-
-	Usuario validarUsuarioLogin(String email, String senha);
-}
--- src/main/java/br/com/ans/model/EntidadeBase.java
+++
@@ -1,5 +0,0 @@
-package br.com.ans.model;
-
-public interface EntidadeBase {
-	public Long getId();
-}
